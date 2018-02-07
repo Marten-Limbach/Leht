46095c3be232f4e807073aac4eef88cfe9044b01
kristo-j <kristo.j@hotmail.com>
2018-02-08 00:17:09 +0200

    Sidebar scroller

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  var windw = this;
+
+  $.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+  };
+
+$('.sidebarDiv').followTo(250);
+});
